88bfa649c271710d5aeb8e050b658ad669c0c554
Simone <simone.giacomelli@gmail.com>
2023-03-17 19:25:26 +0000

    uncommited

--- src/jvmMain/kotlin/me.simone.application/Server.kt
+++ src/jvmMain/kotlin/me.simone.application/Server.kt
@@ -8,22 +8,47 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.int
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import java.io.File
 import java.net.URL
+import kotlin.math.roundToInt
 import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
+
+
+private val LocalDateTime.hour_pad: String get() = "$hour".padStart(2, '0')
 
 @Serializable
 data class Row(val date: LocalDateTime, val wind: Int, val gusts: Int)
 
 fun main() {
     val windDb = WindDb("wind.json")
-    windDb.fetch(19000)
     val elements = windDb.rows()
     println("${elements.size} elements")
-    elements.groupBy { it.date.year }.map {
-        println("${it.key} ${it.value.size}")
-    }
+    val limit = LocalDateTime(2023, 1, 15, 0, 0)
+    elements
+        .filter { it.date > limit }
+//        .filter { it.date.hour in 9..17 }
+        .groupBy { it.date.run { Pair(it.date.dayOfMonth, it.date.hour) } }
+        .map {
+            val dayOfMont = it.key.first
+            val hour = it.key.second
+            val windAverage = it.value.map { it.wind }.average().roundToInt()
+            val gustsAverage = it.value.map { it.gusts }.average().roundToInt()
+            "$dayOfMont $hour $windAverage $gustsAverage".split(" ").joinToString("\t")
+        }.joinToString("\n")
+        .let { "dayOfMont hour windAverage gustsAverage".split(" ").joinToString("\t") + "\n" + it }
+        .also {
+            copyToClipboard(it)
+            println(it)
+            File("out.tsv").writeText(it)
+        }
+}
+
+fun copyToClipboard(string: String) {
+    Toolkit.getDefaultToolkit()
+        .systemClipboard
+        .setContents(StringSelection(string), null)
 }
 
 fun parseDate(content: String): LocalDateTime {
